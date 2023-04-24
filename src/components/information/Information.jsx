707c4d0417413a52c8e9f532d850699ae0186838
emcofah <emfh21@student.bth.se>
2023-04-24 11:32:54 +0200

    added lazy loading, srcset and size to images

--- src/components/information/Information.jsx
+++ src/components/information/Information.jsx
@@ -12,10 +12,34 @@ function Information() {
             <h2>Information</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, dolorem asperiores quod ea odio dicta dolore dolores necessitatibus quos, sequi debitis aut et quibusdam. Aspernatur mollitia culpa aut eum iure.</p>
             <div className="image-container">
-                <img src={Lion} alt="Lion" />
-                <img src={Gorilla} alt="Gorilla" />
-                <img src={Butterfly} alt="Butterfly" />
-                <img src={Polarbear} alt="Polarbear" />
+                <img
+                    src={Lion}
+                    alt="Lion"
+                    loading="lazy"
+                    srcSet={`${Lion} 400w, ${Lion} 800w, ${Lion} 1200w`}
+                    sizes="(max-width: 600px) 100vw, (max-width: 768px) 50vw, 33.3vw"
+                />
+                <img
+                    src={Gorilla}
+                    alt="Gorilla"
+                    loading="lazy"
+                    srcSet={`${Gorilla} 400w, ${Gorilla} 800w, ${Gorilla} 1200w`}
+                    sizes="(max-width: 600px) 100vw, (max-width: 768px) 50vw, 33.3vw"
+                />
+                <img
+                    src={Butterfly}
+                    alt="Butterfly"
+                    loading="lazy"
+                    srcSet={`${Butterfly} 400w, ${Butterfly} 800w, ${Butterfly} 1200w`}
+                    sizes="(max-width: 600px) 100vw, (max-width: 768px) 50vw, 33.3vw"
+                />
+                <img
+                    src={Polarbear}
+                    alt="Polarbear"
+                    loading="lazy"
+                    srcSet={`${Polarbear} 400w, ${Polarbear} 800w, ${Polarbear} 1200w`}
+                    sizes="(max-width: 600px) 100vw, (max-width: 768px) 50vw, 33.3vw"
+                />
             </div>
             <div className="special-font__container">
                 <p className='special-font'>Lorem ipsum</p>
